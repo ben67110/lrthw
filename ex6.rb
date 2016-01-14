4e2a30feba0c3b444ae520c319c46c9d6dfd9ac0
martinezmanuel <benjamin.h.67110@gmail.com>
2016-01-14 16:33:45 +0100

    push de l'exo 6

---
+++ ex6.rb
@@ -0,0 +1,20 @@
+types_of_people = 10 #coucou
+x = "There are #{types_of_people} types of people." #caca
+binary = "binary" #cucu
+do_not = "don't" #coco
+y = "Those who know #{binary} and those who #{do_not}." #cici
+
+puts x #bonjour
+puts y #Shalom
+puts "I said: #{x}." #Aloha
+puts "I also said: '#{y}' " #Salam
+
+hilarious = false #A
+joke_evaluation = "Isn't that joke so funny?! #{hilarious}" #B
+
+puts joke_evaluation #c
+
+w = "This is the left side of..." #d
+e = "a string with a right side." #e
+
+puts w + e #f
